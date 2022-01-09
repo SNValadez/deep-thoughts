f2f9a3f1ba37f1c3f40be29e2245f0a47625498e
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-08 19:11:36 -0800

    update types and source

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,14 +3,24 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
-type Thought {
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
+    reactions: [Reaction]
   }
-type Query {
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
+  type Query {
     thoughts(username: String): [Thought]
   }
 `;
