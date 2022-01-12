84286a7e0549ed7e9b7a5933b85631bf51a35caf
S. N. Valadez <88351213+SNValadez@users.noreply.github.com>
2022-01-11 22:52:47 -0800

    import auth for header and app

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
+import { setContext } from '@apollo/client/link/context';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 
@@ -16,8 +17,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
